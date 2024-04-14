e598d81581e6da556f8a5a9e5b106dfae6ffb14f
Taka Oyama <toyama4649@hotmail.com>
2024-04-14 23:34:58 +0900

    fix type

--- src/Events/StatementExecuted.php
+++ src/Events/StatementExecuted.php
@@ -3,18 +3,18 @@
 namespace Kirameki\Database\Events;
 
 use Kirameki\Database\Connection;
-use Kirameki\Database\Statement;
+use Kirameki\Database\Schema\Statements\SchemaStatement;
 
 class StatementExecuted extends DatabaseEvent
 {
     /**
-     * @template TStatement of Statement
+     * @template TSchemaStatement of SchemaStatement
      * @param Connection $connection
-     * @param TStatement $statement
+     * @param TSchemaStatement $statement
      */
     public function __construct(
         Connection $connection,
-        public readonly Statement $statement,
+        public readonly SchemaStatement $statement,
     )
     {
         parent::__construct($connection);
